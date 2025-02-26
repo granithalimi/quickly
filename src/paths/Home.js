d2719f65f6e168fc68444481b930de3c188122d1
Granit Halimi <granithalimi@Granits-MacBook-Air.local>
2025-02-26 21:20:36 +0100

    random shit

--- src/paths/Home.js
+++ src/paths/Home.js
@@ -1,4 +1,4 @@
-import { React, useContext} from 'react'
+import { React, useContext, useEffect, useState} from 'react'
 import Header from '../components/Header'
 import Slider from '../components/Slider'
 import { ColorContext } from '../App'
@@ -6,9 +6,28 @@ import SubHeader from '../components/SubHeader';
 
 function Home() {
   const [darkMode] = useContext(ColorContext);
+  const [products, setproducts] = useState([])
+
+  useEffect(() => {
+    console.log("rerendered")
+  })
+  
+  useEffect(() => {
+    // check if the json is working...
+    fetch('https://dummyjson.com/test')
+    .then(res => res.json())
+    .then(data => {
+      (data.status == "ok") &&
+      // fetch the data if the json is working...
+      fetch('https://dummyjson.com/products/categories')
+      .then(res => res.json())
+      .then(data => setproducts(data));
+    });
+  }, [])
+  
 
   return (
-    <div className={`${darkMode ? "bg-gray-700" : ""} h-[3000px]`}>
+    <div className={`${darkMode ? "bg-gray-700" : ""}`}>
       <Header />
       <SubHeader />
       <Slider />
